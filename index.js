21b01f056ac0e5ecf832f5e1c8108ad5a3474701
KuuLotus <kuulotusy@gmail.com>
2023-04-20 01:42:10 +0900

    [add]DELETE

--- index.js
+++ index.js
@@ -66,6 +66,12 @@ app.put("/products/:id", async (req, res) => {
   res.redirect(`/products/${product._id}`);
 });
 
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleteProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
 app.listen(3000, () => {
   console.log("ポート３０００で待受中・・・");
 });
